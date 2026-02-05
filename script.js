d0dbeecba9cde4884313ddfee9db50ddeede0475
Michael2211967 <106823044+Michael2211967@users.noreply.github.com>
2026-02-05 16:29:53 +0100

    build in function to wrap the output after 40 chars

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+function wrapHtmlText(text) {
+    let segmentLength = 40;
+
+    let regex = new RegExp('.{1,' + segmentLength + '}', 'g');
+
+    // Hier nutzen wir <br> statt \n
+    let htmlOutput = text.match(regex).join('<br>');
+    return htmlOutput;
+}
 function run() {
     let kiloprice = parseFloat(document.getElementById('kiloprice').value) || 0;
     let kiloquantity = parseFloat(document.getElementById('kiloquantity').value) || 0;
@@ -8,7 +17,7 @@ function run() {
     let quantity = area * kiloquantity;
     let price = quantity * kiloprice;
 
-    document.getElementById('area').innerText = `FUER DIE FLAECHE VON ${area.toFixed(2)} QM`;
-    document.getElementById('quantity').innerText = `WERDEN ${quantity.toFixed(2)} KILO FARBE BENOETIGT.`;
-    document.getElementById('price').innerText = `DER PREIS FUER DIESE ${quantity.toFixed(2)} KG FARBE BETRAEGT ${price.toFixed(2)} EUR.`;
+    document.getElementById('area').innerHTML = wrapHtmlText(`FUER DIE FLAECHE VON ${area.toFixed(2)} QM`);
+    document.getElementById('quantity').innerHTML = wrapHtmlText(`WERDEN ${quantity.toFixed(2)} KILO FARBE BENOETIGT.`);
+    document.getElementById('price').innerHTML = wrapHtmlText(`DER PREIS FUER DIESE ${quantity.toFixed(2)} KG FARBE BETRAEGT ${price.toFixed(2)} EUR.`);
 }
