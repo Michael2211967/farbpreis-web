ac382896a6edfda61377db96c3537c614c50c266
Michael2211967 <106823044+Michael2211967@users.noreply.github.com>
2026-02-12 23:09:12 +0100

    built in tax

--- script.js
+++ script.js
@@ -12,12 +12,15 @@ function run() {
     let kiloquantity = parseFloat(document.getElementById('kiloquantity').value) || 0;
     let length = parseFloat(document.getElementById('length').value) || 0;
     let width = parseFloat(document.getElementById('width').value) || 0;
+    let tax_rate = parseFloat(document.getElementById('tax-rate').value) || 0;
 
     let area = length * width;
     let quantity = area * kiloquantity;
     let price = quantity * kiloprice;
+    let tax = price * tax_rate / 100;
 
     document.getElementById('area').innerHTML = wrapHtmlText(`FUER DIE FLAECHE VON ${area.toFixed(2)} QM`);
     document.getElementById('quantity').innerHTML = wrapHtmlText(`WERDEN ${quantity.toFixed(2)} KILO FARBE BENOETIGT.`);
     document.getElementById('price').innerHTML = wrapHtmlText(`DER PREIS FUER DIESE ${quantity.toFixed(2)} KG FARBE BETRAEGT ${price.toFixed(2)} EUR.`);
+    document.getElementById('tax').innerHTML = wrapHtmlText(`DIE MEHRWERTSTEUER FUER DIESE ${price.toFixed(2)} EUR BETRAEGT ${tax.toFixed(2)} EUR.`);
 }
